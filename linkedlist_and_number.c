91db295073532de6b28bf8fc023ae6091e742c51
Venkata-Lakshmi-Nainar <46933413+Venkata-Lakshmi-Nainar@users.noreply.github.com>
2019-01-24 13:05:09 +0530

    Update linkedlist_and_number.c

--- linkedlist_and_number.c
+++ linkedlist_and_number.c
@@ -12,81 +12,95 @@ Output :
 3 -> 4 -> 6 -> 7 -> 8 -> 32 -> 40 -> 45
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-    int data ;
-    struct node *link ;
+struct node{
+  int data ;
+  struct node *link ;
 };
 
 struct node* create ( int num ) {
-    struct node *x = ( struct node * ) malloc (sizeof(struct node)) ;
-    x -> data = num ;
-    x -> link = NULL ;
-    return x ;
+  struct node* x = ( struct node * ) malloc ( sizeof ( struct node ) ) ;
+  x -> data = num ;
+  x -> link = NULL ;
+  return x ;
 }
 
 void add ( struct node **start , int num ) {
-    struct node *temp = create (num) ;
-    if ( *start == NULL ) {
-        *start = temp ;
-    }else{
-        temp -> link = *start ;
-        *start = temp ;
-    }
+  struct node *temp = create ( num ) ;
+  if ( *start == NULL ) {
+    *start = temp ;
+  }else{
+    temp -> link = *start ;
+    *start = temp ;
+  }
 }
 
 void display ( struct node *head ) {
-    while ( head != NULL ) {
-        printf ( "%d -> " , head -> data ) ;
-        head = head -> link ;
-    }
-    printf ("NULL\n") ;
+  while ( head != NULL ) {
+    printf ( "%d -> " , head -> data ) ;
+    head = head -> link ;
+  }
+  printf ("NULL \n") ;
 }
 
-int add_to_list ( struct node *t , int x ) {
-    int carry , base ;
-    if ( t -> link == NULL ) {
-        carry = t -> data + x ;
-        if ( carry > 9 ) {
-            t -> data = carry % 10 ;
-            return carry/10 ;
-        }
-        t -> data = carry ;
-        return 0 ;
-    }
-    carry = add_to_list ( t -> link , x ) ; 
-    if ( ( t -> data + carry ) > 9 ) {
-        base = (t -> data + carry ) / 10 ;
-        t -> data = (t -> data + carry ) % 10 ;
-        return base ; 
-        
-    }
-    t -> data = t -> data + carry ;
-    return 0;
-}
-struct node* addition_to_list( struct node **begin , int x ) {
-    int carry = add_to_list (*begin,x) ;
-    if ( carry > 0 ) {
-        struct node *new_head = ( struct node * ) malloc (sizeof(struct node)) ;
-        new_head -> data = carry ;
-        new_head -> link = *begin ;
-        *begin = new_head ;
-    }
-    return *begin ;
-}
+struct node* merge_list ( struct node* list1 , struct node* list2 ) {
+  struct node *list_head, *p , *q , *prev , *temp;
+  if ( list1 == NULL ) {
+    return list2 ;
+  }
+  if ( list2 == NULL ) {
+    return list1 ;
+  }
+  if ( ( list1 -> data ) < ( list2 -> data ) ) {
+    list_head = list1 ;
+    q = list2 ;
+  }
+  else {
+    list_head = list2 ;
+    q = list1 ;
+  }
+  p = list_head ;
 
+  while ( ( p != NULL ) && ( q != NULL ) )  {
+    if ( (q -> data) > (p -> data) ){
+      prev = p ;
+      p = p -> link ;
+    }
+    else {
+      temp = q -> link ;
+      prev -> link = q ;
+      prev = prev -> link ;
+      prev -> link = p ;
+      p = prev ;
+      q = temp ;
+    }
+  }
+
+if ( p == NULL ) {
+  prev -> link = q ;
+}
+return list_head ;
+}
 
 int main(){
-    struct node *parent_list = NULL ;
-    int x = 10000 ;
-    add ( &parent_list , 9) ;
-    add ( &parent_list , 9) ;
-    add ( &parent_list , 9) ;
-    display(parent_list);
-    parent_list = addition_to_list(&parent_list,x) ;
-    display(parent_list);
-    return 0 ;
+  struct node *list_1 = NULL , *list_2 = NULL ;
+  add ( &list_1 , 78 ) ;
+  add ( &list_1 , 67 ) ;
+  add ( &list_1 , 54 ) ;
+  add ( &list_1 , 35 ) ;
+  add ( &list_1 , 22 ) ;
+  add ( &list_1 , 9 ) ;
+  display(list_1) ;
+
+  add ( &list_2 , 65 ) ;
+  add ( &list_2 , 44 ) ;
+  add ( &list_2 , 10 ) ;
+  add ( &list_2 , 1 ) ;
+  display(list_2) ;
+
+  display(merge_list(list_1,list_2)) ;
+
+  return 0;
 }
